93f394fb65c697c2e214eeedafbaadfec8cbdc68
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-02-08 10:09:15 +0530

    Added `Ques1` For Lab 5

---
+++ Lab 5 - Javascript/Ques1/script.js
@@ -0,0 +1,13 @@
+const number = parseInt(prompt('Enter the number N: '));
+if (number.length <= 0 || isNaN(number)) {
+    alert('Invalid Input!! Please try again');
+    document.location.reload(true);
+}
+const text = "The result of Sum of ";
+document.getElementById("number2").innerHTML = text + number + " numbers is:";
+let sum = 0;
+
+for (let i = 1; i <= number; i++) {
+    sum += i;
+}
+document.getElementById("result2").value = sum;
