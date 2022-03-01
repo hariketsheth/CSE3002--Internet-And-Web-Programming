29585eb5d9aa528f8446d52add7220f83d9aaa2c
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-03-01 13:14:12 +0530

    Updated `Lab 7 Codes`

--- Lab 7 - Hackathon/Ques3/main.js
+++ Lab 7 - Hackathon/Ques3/main.js
@@ -3,16 +3,18 @@ const username = document.getElementById('user-name');
 const fullname = document.getElementById('full-name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const theme = document.getElementById('theme');
 const submit = document.getElementById('submit')
 
 submit.addEventListener('click', () => {
   var c1 = false,
     c2 = false,
     c3 = false,
-    c4 = false;
+    c4 = false,
+    c5 = false;
   const usernameValue = username.value;
   let user_reg=/^[A-Za-z][A-Za-z0-9_]{5,29}$/;
-  if (usernameValue === ''|| !usernameValue.match(user_reg)) {
+  if (!usernameValue.match(user_reg)) {
     document.getElementById('error-msg-user').style.display = 'block';
     document.getElementById('check1').style.display = 'none';
     document.getElementById('error1').style.display = 'block';
@@ -26,7 +28,7 @@ submit.addEventListener('click', () => {
 
   const fullnameValue = fullname.value;
   let full_name=/^[A-Za-z][A-Za-z0-9_\s]{5,29}$/;
-  if (fullnameValue === ''|| !fullnameValue.match(full_name)) {
+  if (!fullnameValue.match(full_name)) {
     document.getElementById('error-msg-full').style.display = 'block';
     document.getElementById('check2').style.display = 'none';
     document.getElementById('error2').style.display = 'block';
@@ -40,7 +42,7 @@ submit.addEventListener('click', () => {
 
   const emailValue = email.value;
   let isEmail = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,15}$/;
-  if (emailValue === '' || (!emailValue.match(isEmail))) {
+  if (!emailValue.match(isEmail)) {
     document.getElementById('error-msg-email').style.display = 'block';
     document.getElementById('check3').style.display = 'none';
     document.getElementById('error3').style.display = 'block';
@@ -54,7 +56,7 @@ submit.addEventListener('click', () => {
 
   const passwordValue = password.value;
   let isPassword = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-  if (passwordValue === '' || (!passwordValue.match(isPassword))) {
+  if (!passwordValue.match(isPassword)) {
     document.getElementById('error-msg-pass').style.display = 'block';
     document.getElementById('check4').style.display = 'none';
     document.getElementById('error4').style.display = 'block';
@@ -65,10 +67,23 @@ submit.addEventListener('click', () => {
     document.getElementById('error4').style.display = 'none';
     c4 = true;
   }
-
-  if (c1 && c2 && c3 && c4) {
-    alert('Form submitted successfully !! \n\nDetails submitted are as follows: \n1. Username: ' + usernameValue +
-      ' \n2. Full Name: ' + fullnameValue + ' \n3. Email: ' + emailValue);
+  const themeValue = theme.value;
+  let themereg = /^([1-7])$/;
+  if (!themeValue.match(themereg)) {
+    document.getElementById('error-msg-theme').style.display = 'block';
+    document.getElementById('check5').style.display = 'none';
+    document.getElementById('error5').style.display = 'block';
+    c5 = false;
+  } else {
+    document.getElementById('error-msg-theme').style.display = 'none';
+    document.getElementById('check5').style.display = 'block';
+    document.getElementById('error5').style.display = 'none';
+    c5 = true;
+  }
+  const themeText = theme.options[theme.selectedIndex].text;
+  if (c1 && c2 && c3 && c4 && c5) {
+    alert('Team registered successfully !! \n\nDetails submitted are as follows: \n1. Username: ' + usernameValue +
+      ' \n2. Full Name: ' + fullnameValue + ' \n3. Email: ' + emailValue+' \n4. Theme: '+themeText);
     window.location.replace("success.html");
   }
 
