9eb06f41c79a5656ed3654df61b8985826974ea6
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-03-19 08:20:58 +0530

    Added `Lab 8` Codes

---
+++ LAB/Lab 9 - PHP/index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 9 - PHP</title>
+</head>
+<body>
+    <h1>Lab 9 - PHP</h1>
+    <p> Design a user-interactive website  for taking in inputs from the user for the loan amount at <br>
+    a particular rate of interest. Display the yearly loan amount to be paid by the user for the entered loan amount.</p>
+
+    <form action="" method="POST" >
+        <input type="text" placeholder="Enter the loan amount" id="loan" name="loan">
+        <input type="text" placeholder="Enter the rate of interest" name="interest" id="interest">
+        <input type="text" placeholder="Press Get Amount to see the amount" name="amount" id="amount">
+        <input type="submit" value="Get Amount">
+    </form>
+</body>
+</html>
