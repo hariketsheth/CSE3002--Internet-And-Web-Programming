7d6bb6e78b2d6083175aa183cbb6dcd3aae0f8a9
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-03-19 08:47:01 +0530

    Added `Lab 9 Codes`

--- LAB/Lab 9 - PHP Execution/index.php
+++ LAB/Lab 9 - PHP Execution/index.php
@@ -20,7 +20,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Lab 9 - PHP COnditionals</title>
+    <title>Lab 9 - PHP Conditionals</title>
 </head>
 <body>
     <form action="" method="POST" id="php-conditional" name="php-conditional">
